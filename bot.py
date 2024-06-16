b5917cc39ee204848a5917e5ea824777d4abec29
sagato-bots-official-787 <596c29c9e5@genvolvo.lol>
2024-06-16 16:05:29 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Straw_Hat_Gc")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Urr_sanjii")
         self.LOGGER(__name__).info(f"""
 ──╔╦══╦═══╦╗─╔╦╗─╔╦══╗╔═══╦════╦════╗
 ──║╠╣╠╣╔═╗║║─║║║─║║╔╗║║╔═╗║╔╗╔╗╠══╗═║
--- config.py
+++ config.py
@@ -15,18 +15,18 @@ APP_ID = int(os.environ.get("APP_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
 
-OWNER = os.environ.get("OWNER", "") #Owner username
+OWNER = os.environ.get("OWNER", "urr_sanjii") #Owner username
 OWNER_ID = int(os.environ.get("OWNER_ID", "6727550037")) #Owner user id
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://zorosan563:zoro@zorocluster00001.evjzkeb.mongodb.net/?retryWrites=true&w=majority&appName=zorocluster00001")
 DB_NAME = os.environ.get("DB_NAME", "sagatoanime")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002079972903"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", ""))
 
 
-SECONDS = int(os.getenv("SECONDS", "1800")) # auto delete in seconds
+SECONDS = int(os.getenv("SECONDS", "7200")) # auto delete in seconds
 
 
 
@@ -40,7 +40,7 @@ START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store privat
 
 try:
     ADMINS=[6727550037]
-    for x in (os.environ.get("ADMINS", "6727550037 7162615398").split()):
+    for x in (os.environ.get("ADMINS", "6727550037 7162615398 1865103287 1675155069").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
